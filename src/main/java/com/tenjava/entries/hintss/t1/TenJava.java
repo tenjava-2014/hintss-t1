0c0acb64ebf82127dd7d9cde8c598043aa5493ae
Henry Ammermann <hintsss@gmail.com>
2014-07-11 17:15:03 -0700

    add onenables and stuffs lol

--- src/main/java/com/tenjava/entries/hintss/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/hintss/t1/TenJava.java
@@ -2,4 +2,14 @@ package com.tenjava.entries.hintss.t1;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
